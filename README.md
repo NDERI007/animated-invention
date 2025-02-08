a1d14edc91ebd643f609a781120f10fdc8036647
NDERI007 <tnderi373@gmail.com>
2025-02-08 16:59:41 +0300

    grids created not yet done

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # animated-invention
+Side -project called etch-a-skecth
+As the name suggests you can sketch to your hearts desire
---
+++ etch-sk.js
@@ -0,0 +1,12 @@
+ 
+/* eslint-disable no-undef */ 
+const container = document.querySelector("#umbrella");
+function makeGrid(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement("div");
+        container.appendChild(cell).className = "grid-item";
+    };
+}
+makeGrid(16, 16);
