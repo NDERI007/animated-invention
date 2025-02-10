0c81566c866dd6548451dce8db45fc053b2ac788
NDERI007 <tnderi373@gmail.com>
2025-02-10 11:52:47 +0300

    code changed for creating the square grid

--- etch-sk.js
+++ etch-sk.js
@@ -1,12 +1,37 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
  
 /* eslint-disable no-undef */ 
 const container = document.querySelector("#umbrella");
-function makeGrid(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
+let color;
+function makeGrid(size) {
+    let cells = container.querySelectorAll("div");
+    cells.forEach(div => div.remove());
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    let amount = size * size;
+    for (c = 0; c < amount; c++) {
         let cell = document.createElement("div");
-        container.appendChild(cell).className = "grid-item";
+        cell.addEventListener("mouseover", colourSquare);
+        container.insertAdjacentElement("beforeend", cell);
     };
 }
-makeGrid(16, 16);
+makeGrid(16);
+
+function changeSize(input) {
+    if (input >= 2 && input <= 100) {
+        makeGrid(input);
+    } else {
+        console.log("ERROR")
+    }
+}
+function colourSquare() {
+    if (color === "random") {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+    } else {
+        this.style.backgroundColor = color;
+    }
+
+}
+function changeColor(choice) {
+    color = choice;
+}
